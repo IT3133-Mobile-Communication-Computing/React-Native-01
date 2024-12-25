5bb493ca9fe36199b55bd6e74c2ffebf828efe7d
DilmyPerera <samadiniperera20@gmail.com>
2024-12-25 09:37:40 +0530

    ✨feature: import Text component from react-native-paper

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { PaperProvider } from "react-native-paper";
+import { Text } from "react-native-paper";
 
 export default function App() {
   return (
