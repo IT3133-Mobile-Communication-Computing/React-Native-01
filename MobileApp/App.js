0b7e585ec74d508aa4bbfdc298e5e6f35925d18e
DilmyPerera <samadiniperera20@gmail.com>
2024-12-25 09:52:19 +0530

    ✨feature: import and add Home component to App.js

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { PaperProvider } from "react-native-paper";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <PaperProvider>
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <Home />
+      </View>
     </PaperProvider>
   );
 }
