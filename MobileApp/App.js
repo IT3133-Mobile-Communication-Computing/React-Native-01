77adc32357259a0fa069b70d5a983a3872072421
DilmyPerera <samadiniperera20@gmail.com>
2024-12-25 10:03:38 +0530

    ✨feature: import SafeAreaView for handling safe area insets

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, ScrollView } from "react-native";
 import { PaperProvider } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Home from "./components/Home";
 
 export default function App() {
   return (
     <PaperProvider>
-      <ScrollView>
-        <View style={styles.container}>
-          <Home />
-          <StatusBar style="auto" />
-        </View>
-      </ScrollView>
+      <SafeAreaView>
+        <ScrollView>
+          <View style={styles.container}>
+            <Home />
+            <StatusBar style="auto" />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </PaperProvider>
   );
 }
