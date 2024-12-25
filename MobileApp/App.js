621793e8e527918eda27d4df3438d2317b7e0e8d
DilmyPerera <samadiniperera20@gmail.com>
2024-12-25 09:51:11 +0530

    ✨feature:implement Home component with styled text and divider

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,39 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { PaperProvider } from "react-native-paper";
-import { Text } from "react-native-paper";
 
 export default function App() {
   return (
     <PaperProvider>
-      <View style={styles.container}>
-        <Text variant="headlineLarge">Headline Large</Text>
-        <Divider />
-        <Text variant="bodyMedium">
-          The shimmering light of the moon danced upon the rippling surface of
-          the tranquil lake, casting a silvery glow across the landscape.
-          Beneath the ancient oak tree, a curious fox watched, its ears
-          twitching at the faint sounds of the forest. Somewhere in the
-          distance, the hoot of an owl broke the serene silence, a reminder of
-          the mysteries hidden in the shadowy depths of the night. As the breeze
-          carried the scent of blooming jasmine, time seemed to pause, leaving
-          the world in a dreamlike stillness."
-        </Text>
-        <StatusBar style="auto" />
-      </View>
+      <View style={styles.container}></View>
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  body: {
-    padding: 8,
-    textAlign: "justify",
-  },
-});
---
+++ MobileApp/components/Home.js
@@ -0,0 +1,35 @@
+import { StyleSheet, View } from "react-native";
+import { Text, Divider } from "react-native-paper";
+
+export default function Home() {
+  return (
+    <>
+      <Text variant="headlineLarge">Headline Large</Text>
+      <Divider />
+      <Text variant="bodyMedium" style={styles.body}>
+        The shimmering light of the moon danced upon the rippling surface of the
+        tranquil lake, casting a silvery glow across the landscape. Beneath the
+        ancient oak tree, a curious fox watched, its ears twitching at the faint
+        sounds of the forest. Somewhere in the distance, the hoot of an owl
+        broke the serene silence, a reminder of the mysteries hidden in the
+        shadowy depths of the night. As the breeze carried the scent of blooming
+        jasmine, time seemed to pause, leaving the world in a dreamlike
+        stillness."
+      </Text>
+      <StatusBar style="auto" />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  body: {
+    padding: 8,
+    textAlign: "justify",
+  },
+});
