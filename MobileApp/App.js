f699530464fac4c588bb339d016a0cfdb95fcb20
DilmyPerera <samadiniperera20@gmail.com>
2024-12-25 09:58:43 +0530

    ✨feature: add ScrollView component for scrollable content

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import { PaperProvider } from "react-native-paper";
 import Home from "./components/Home";
 
 export default function App() {
   return (
     <PaperProvider>
-      <View style={styles.container}>
-        <Home />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Home />
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
     </PaperProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
